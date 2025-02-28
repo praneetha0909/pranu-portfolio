1922b8c4b428cf8794e690ac74112a4b12fcf4f2
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-02-28 09:16:59 -0600

    Update Encryption.tsx

--- components/main/Encryption.tsx
+++ components/main/Encryption.tsx
@@ -13,12 +13,10 @@ const Encryption = () => {
           variants={slideInFromTop}
           className="text-[40px] font-medium text-center text-gray-200"
         >
-          Performance
           <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">
             {" "}
-            &{" "}
+            About me{" "}
           </span>
-          Security
         </motion.div>
       </div>
 
