d40ffe1538ac19559d60fae62fe022917ca53f26
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-02-27 23:36:16 -0600

    Update Navbar.tsx

--- components/main/Navbar.tsx
+++ components/main/Navbar.tsx
@@ -11,7 +11,7 @@ const Navbar = () => {
           className="h-auto w-auto flex flex-row items-center"
         >
           <Image
-            src="/NavLogo1.png"
+            src="/logo.png"
             alt="logo"
             width={70}
             height={70}
