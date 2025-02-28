7274305fa47a6a273505ec62cf0a25591affdb69
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-02-28 00:09:58 -0600

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -30,7 +30,7 @@ const HeroContent = () => {
 
         <motion.div
           variants={slideInFromLeft(0.5)}
-          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[500px] w-auto h-auto"
+          className="flex flex-col gap-6 mt-6 text-6xl font-bold text-white max-w-[400px] w-auto h-auto"
         >
           <span>
             <span className="text-transparent bg-clip-text bg-gradient-to-r from-orange-500 to-purple-500">
