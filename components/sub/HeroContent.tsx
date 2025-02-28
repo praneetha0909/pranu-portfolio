879ce873418a28694a1c19e027c1cda4f4dff712
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-02-28 17:28:45 -0600

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -23,9 +23,9 @@ const HeroContent = () => {
       <div className="h-full w-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
           variants={slideInFromTop}
-          className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
+          className="Welcome-box py-[8px] px-[7px] border border-[#d19932] opacity-[0.9]"
         >
-          <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
+          <SparklesIcon className="text-[#d19932] mr-[10px] h-5 w-5" />
           <h1 className="Welcome-text text-[13px]">
             Software Development Engineer
           </h1>
