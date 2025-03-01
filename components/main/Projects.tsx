55d09cb3440e98d3be780ed2418577594be077fc
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-03-01 00:25:28 -0600

    Update Projects.tsx

--- components/main/Projects.tsx
+++ components/main/Projects.tsx
@@ -12,12 +12,12 @@ const Projects = () => {
         <ProjectCard
           src="/uta.png"
           title="University of Texas at Arlington"
-          description={'Master's in Computer Science (2023 - 2024)\n-Course Work: Design and Analysis of Algorithms, Cloud computing & Big data, Database systems, Artificial Intelligence,  Machine Learning, Wed Data Management, Distributed systems'}
+          description={"Master's in Computer Science (2023 - 2024)\n-Course Work: Design and Analysis of Algorithms, Cloud computing & Big data, Database systems, Artificial Intelligence,  Machine Learning, Wed Data Management, Distributed systems"}
         />
         <ProjectCard
           src="/vignan.png"
           title="Vignan Institute of Technology & Science"
-          description={'Bachelor's in Computer Science (2016 - 2020)\n-Course Work: Python, Java, C, C++, Data Mining, Software Engineering'}
+          description={"Bachelor's in Computer Science (2016 - 2020)\n-Course Work: Python, Java, C, C++, Data Mining, Software Engineering"}
         />
       </div>
       {/* Section 2: Work Experience */}
@@ -28,23 +28,23 @@ const Projects = () => {
         <ProjectCard
           src="/reality.png"
           title="RealityAI"
-          description={'Software Developer (January 2025 - Present)\n-Engineered scalable ReactJS interfaces for Marvel AI and Sky AI analytics and automation tools, integrating Node.js
+          description={"Software Developer (January 2025 - Present)\n-Engineered scalable ReactJS interfaces for Marvel AI and Sky AI analytics and automation tools, integrating Node.js
 & Python backend services, resulting in 30% faster load times and 20% increased AI adoption\n-Designed and catalyzed RESTful APIs with Flask to enable real-time data processing and cloud service integrations,
-reducing latency by 30% and increasing data throughput by 45%'}
+reducing latency by 30% and increasing data throughput by 45%"}
         />
         <ProjectCard
           src="/tom.jpg"
           title="Tomorrows AI"
-          description={'Software Developer (September 2024 - December 2024)\n-Pioneered microservices architecture with MySQL and AWS S3 to process AI data, reducing data latency by 40
+          description={"Software Developer (September 2024 - December 2024)\n-Pioneered microservices architecture with MySQL and AWS S3 to process AI data, reducing data latency by 40
 milliseconds; deployed on 100+ nodes to improve scalability\n-Constructed an ML-powered recommendation API with Flask, achieving a 30% improvement in response times
-through optimized database queries'}
+through optimized database queries"}
 
         />
         <ProjectCard
           src="/infosys.png"
           title="Infosys"
-          description={'Software Engineer (March 2021 - November 2022)\n-Architected reusable code libraries, reducing development time by 20% and improving scalability\n-Revamped SAP Fiori applications by simplifying navigation complexity, reducing clicks per transaction, and
-improving system response time, saving users 15 minutes per task on average'}
+          description={"Software Engineer (March 2021 - November 2022)\n-Architected reusable code libraries, reducing development time by 20% and improving scalability\n-Revamped SAP Fiori applications by simplifying navigation complexity, reducing clicks per transaction, and
+improving system response time, saving users 15 minutes per task on average"}
         />
       </div>
 
