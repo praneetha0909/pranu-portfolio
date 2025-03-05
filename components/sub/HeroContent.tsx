eea2702171e70d16301e7ce9e33e450265c89fd3
Praneetha Mukkamala <51907247+praneetha0909@users.noreply.github.com>
2025-03-05 17:23:03 -0600

    Update HeroContent.tsx

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -64,7 +64,7 @@ const HeroContent = () => {
         className="w-full h-full flex justify-center items-center"
       >
         <Image
-          src="/mainIconsdark.svg"
+          src="/icons12.jpeg"
           alt="work icons"
           height={650}
           width={650}
